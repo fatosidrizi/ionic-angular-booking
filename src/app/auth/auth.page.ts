cb59cb2c09ef26a691096cc2db175f7fe8024b97
Fatos Idrizi <fatos.idrizi@gmail.com>
2020-04-28 02:00:56 +0200

    On Login: Spinner & Loading Controller.

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
     selector: 'app-auth',
@@ -7,14 +9,33 @@ import {AuthService} from './auth.service';
     styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+    isLoading = false;
 
-    constructor(private authService: AuthService) {
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private loadingController: LoadingController
+    ) {
     }
 
     ngOnInit() {
     }
 
     onLogin() {
+        this.isLoading = true;
         this.authService.login();
+        this.loadingController.create({
+            keyboardClose: true,
+            message: 'Logging in...'
+        }).then(loadingEl => {
+            loadingEl.present();
+            setTimeout(() => {
+                this.isLoading = false;
+                loadingEl.dismiss();
+                this.router.navigateByUrl('/places/tabs/discover');
+            }, 1500);
+        })
+
+
     }
 }

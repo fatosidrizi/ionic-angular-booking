26493882bfd32ff2357d1557d0fe0efb06fe8551
Fatos Idrizi <fatos.idrizi@gmail.com>
2020-04-29 23:46:06 +0200

    Edit Offer Form with Reactive Validation

--- src/app/places/offers/edit-offer/edit-offer.module.ts
+++ src/app/places/offers/edit-offer/edit-offer.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { EditOfferPageRoutingModule } from './edit-offer-routing.module';
+import {EditOfferPageRoutingModule} from './edit-offer-routing.module';
 
-import { EditOfferPage } from './edit-offer.page';
+import {EditOfferPage} from './edit-offer.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    EditOfferPageRoutingModule
-  ],
-  declarations: [EditOfferPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        EditOfferPageRoutingModule,
+        ReactiveFormsModule
+    ],
+    declarations: [EditOfferPage]
 })
-export class EditOfferPageModule {}
+export class EditOfferPageModule {
+}
